62ab465a1c4360ca4c4bc7fd0b5c33a3be7b16bd
Shrey Talreja <shreytalreja25@gmail.com>
2024-05-12 05:31:57 +1000

    Added v0.2 of API with refined prompting

---
+++ backend/test2.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import openai
+
+openai.api_key = "sk-proj-ulcP3hqe7zORRs3HwzT5T3BlbkFJXZVLLvjCU7hBU7Kafppo"
+
+app = Flask(__name__)
+CORS(app)  # This enables CORS for all domains on all routes
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json['prompt']
+    response = chat_with_gpt(user_input)
+    return jsonify({"response": response})
+
+def chat_with_gpt(prompt):
+    modified_prompt = f"Answer as a UNSW assistant: {prompt}. Do not generate any response that is not related to UNSW."
+    response = openai.chat.completions.create(
+        model="ft:gpt-3.5-turbo-1106:personal:unswbot:9Nm4NnHU",
+        messages=[{"role":"user","content":modified_prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+if __name__ == '__main__':
+    app.run(debug=True)
